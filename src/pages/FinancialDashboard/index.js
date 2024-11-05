b512e34ac4a5eafd0f483274bf89e67a3f690b70
brunoo100 <brunodlucas2@gmail.com>
2024-11-05 19:59:38 -0300

    Adiciona filtro de transações por usuário logado

--- src/pages/FinancialDashboard/index.js
+++ src/pages/FinancialDashboard/index.js
@@ -1,8 +1,8 @@
 // src/pages/FinancialDashboard.js
 import React, { useState, useEffect } from "react";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, query, where } from "firebase/firestore";
 import { db } from "../../services/firebase";
-import Header from "../../components/Header"; // Importando o Header
+import { getAuth } from "firebase/auth"; // Importando o Firebase Authentication
 import "./FinancialDashboard.css";
 
 const FinancialDashboard = () => {
@@ -12,10 +12,27 @@ const FinancialDashboard = () => {
   const [expenseTotal, setExpenseTotal] = useState(0);
   const [balance, setBalance] = useState(0);
   const [selectedMonth, setSelectedMonth] = useState(new Date().getMonth() + 1); // Mês atual por padrão
+  const [user, setUser] = useState(null); // Para armazenar o usuário logado
+
+  // Recuperando o usuário logado
+  useEffect(() => {
+    const auth = getAuth();
+    const currentUser = auth.currentUser; // Pega o usuário logado
+    if (currentUser) {
+      setUser(currentUser); // Define o usuário logado
+    }
+  }, []);
 
   useEffect(() => {
     const fetchTransactions = async () => {
-      const querySnapshot = await getDocs(collection(db, "transactions"));
+      if (!user) return; // Se não houver um usuário logado, não faz a busca
+
+      const userTransactionsQuery = query(
+        collection(db, "transactions"),
+        where("userId", "==", user.uid) // Filtrando transações pelo UID do usuário
+      );
+
+      const querySnapshot = await getDocs(userTransactionsQuery);
       const fetchedTransactions = querySnapshot.docs.map((doc) => ({
         id: doc.id,
         ...doc.data(),
@@ -24,7 +41,7 @@ const FinancialDashboard = () => {
     };
 
     fetchTransactions();
-  }, []);
+  }, [user]);
 
   useEffect(() => {
     // Filtrar as transações pelo mês selecionado
@@ -51,9 +68,12 @@ const FinancialDashboard = () => {
     setSelectedMonth(parseInt(event.target.value, 10));
   };
 
+  if (!user) {
+    return <div>Carregando...</div>; // Ou algum tipo de loader ou mensagem de erro
+  }
+
   return (
     <div className="dashboard-container">
-
       <h2>Dashboard Financeiro</h2>
 
       {/* Filtro de mês */}
